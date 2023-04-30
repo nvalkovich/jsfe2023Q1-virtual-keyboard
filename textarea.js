9fdf1a6fa5266cff3164c48420904c58cc0d7666
nvalkovich <anastasiya.valkovich@mail.ru>
2023-04-30 22:15:16 +0300

    feat: add cursor position handling

--- textarea.js
+++ textarea.js
@@ -1,10 +1,31 @@
 class Textarea {
   constructor() {
     this.element = Textarea.createElement();
+    this.element.addEventListener('click', this.setPosition.bind(this));
+    this.position = 0;
+  }
+
+  setPosition() {
+    this.position = this.element.selectionStart;
   }
 
   handleButtonClick(button) {
-    this.element.value += button.en;
+    this.element.focus();
+    switch (button.code) {
+      case 'Enter':
+        this.addSymbol('\n');
+        break;
+      default:
+        this.addSymbol(button.en);
+    }
+    this.position += 1;
+    this.element.selectionStart = this.position;
+    this.element.selectionEnd = this.position;
+  }
+
+  addSymbol(symbol) {
+    const { value } = this.element;
+    this.element.value = value.slice(0, this.position) + symbol + value.slice(this.position);
   }
 
   static createElement() {
